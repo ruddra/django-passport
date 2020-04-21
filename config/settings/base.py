cc68cfa1222920f5157152e37fc275549d1716c1
Arnab Kumar Shil <ruddra90@gmail.com>
2020-04-22 02:02:33 +0600

    Fixed JWT token auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -169,8 +169,7 @@ SOCIALACCOUNT_ADAPTER = "passport.users.adapters.SocialAccountAdapter"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
